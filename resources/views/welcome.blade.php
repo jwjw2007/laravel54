284961bbdeed51cc09429c3215c0d5444df2c1b6
jwjw2007 <2504675637@qq.com>
2017-07-19 13:34:56 +0800

    gx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel54简书</title>
+        <title>Laravel54简书项目</title>
 
         <!-- Fonts -->
         <!-- Styles -->
