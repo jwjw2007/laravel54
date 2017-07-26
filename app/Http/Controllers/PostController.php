43c0dc4ba61d5f07f7b503c4de94d0b7790f8857
jwjw2007 <2504675637@qq.com>
2017-07-26 08:37:10 +0800

    gx

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller {
+
+    //文章列表页
+    public function index() {
+     echo 'fffffffff';
+    }
+
+    //文章详情页
+    public function show() {
+
+    }
+
+    //文章添加页
+    public function create() {
+
+    }
+
+    //文章添加逻辑
+    public function store() {
+
+    }
+
+    //文章编辑页
+    public function edit() {
+
+    }
+
+    //文章编辑逻辑
+    public function update() {
+
+    }
+
+    //文章删除页
+    public function delete() {
+
+    }
+}
