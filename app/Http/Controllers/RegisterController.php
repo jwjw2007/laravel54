d11ca59722c50e5d7f6ad36d66b29dabdd7d2d41
jwjw2007 <2504675637@qq.com>
2017-07-31 13:56:57 +0800

    gx

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,6 +14,18 @@ class RegisterController extends Controller {
 
     //注册行为
     public function register() {
-        
+        //验证
+        $this->validate(request(), [
+            'name' => 'required|min:3|unique:users,name',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required|min:5|confirmed'
+        ]);
+        //业务逻辑
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password'));
+        User::create(compact('name', 'email', 'password'));
+        //渲染
+        return redirect('/login');
     }
 }
