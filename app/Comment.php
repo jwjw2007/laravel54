6e6a37a18af3fd940af739e1999c1da9adc6858f
jwjw2007 <2504675637@qq.com>
2017-08-01 22:23:47 +0800

    gx

--- app/Comment.php
+++ app/Comment.php
@@ -5,5 +5,14 @@ namespace App;
 use App\Model;
 
 class Comment extends Model {
-    //
+    
+    //所属文章
+    public function post() {
+        return $this->belongsTo('App\Post','post_id','id');
+    }
+    
+    //所属用户
+    public function user() {
+        return $this->belongsTo('App\User','user_id','id');
+    }
 }
