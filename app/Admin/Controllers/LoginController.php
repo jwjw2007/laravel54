fa90d3b7e7aec19f4ff5fc6d349a2ecf49309f2d
jwjw2007 <2504675637@qq.com>
2017-08-09 08:51:22 +0800

    gx

---
+++ app/Admin/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+class LoginController extends Controller{
+
+    public function index() {
+        return view('admin.login.index');
+    }
+
+    public function login() {
+        
+    }
+
+    public function logout() {
+        
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,10 @@
 <?php
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/login', function () {
-        return 'This is login';
-    });
+    //登录页
+    Route::get('/login', '\App\Admin\Controllers\LoginController@index');
+    //登录
+    Route::post('/login', '\App\Admin\Controllers\LoginController@login');
+    //登出
+    Route::get('/logout', '\App\Admin\Controllers\LoginController@logout');
 });
