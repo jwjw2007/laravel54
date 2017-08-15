3754f8735c74ba885ded7cece8cec2cdca3c46e8
jwjw2007 <2504675637@qq.com>
2017-08-15 14:02:01 +0800

    gx

--- app/AdminUser.php
+++ app/AdminUser.php
@@ -6,5 +6,6 @@ use App\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class AdminUser extends Authenticatable {
+    //后台管理员登录去掉tolen字段验证
     protected $rememberTokenName = '';
 }
